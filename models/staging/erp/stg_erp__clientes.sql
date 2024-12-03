dea25bfec589b8e8bdb2ee90caad75b4ebb76c60
Bruno Brant <bruno.brant@unimedbh.com.br>
2024-12-03 23:19:34 +0000

    ajuste modelo clientes

--- models/staging/erp/stg_erp__clientes.sql
+++ models/staging/erp/stg_erp__clientes.sql
@@ -1,12 +1,13 @@
 with
     fonte_clientes as (
-        select 
-          cast(ID as varchar) pk_cliente 
-        , cast(CATEGORYNAME as varchar) as nm_empresa_cliente
-        , cast(CITY as varchar) as cidade_cliente
-        , cast(REGION as varchar) as regiao_cliente
-        , cast(COUNTRY as varchar) as pais_cliente
+        select
+            cast(ID as varchar) as pk_cliente
+            , cast(COMPANYNAME as varchar) as nm_empresa_cliente
+            , cast(CITY as varchar) as cidade_cliente
+            , cast(REGION as varchar) as regiao_cliente
+            , cast(COUNTRY as varchar) as pais_cliente
         from {{ source('erp_northwind', 'customer') }}
     )
+
 select *
-from fonte_clientes    
+from fonte_clientes
